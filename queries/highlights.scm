8d9b4e9ecd77956b71d8f24bb4be141160d423e5
Alexandre Pasmantier <alex.pasmant@gmail.com>
2024-07-31 01:21:52 +0200

    fix: update highlights.scm to conform to nvim-tree-sitter standards

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,22 +1,22 @@
-((log_level) @trace
-(#eq? @trace "TRACE"))
+((log_level) @comment.documentation
+(#eq? @comment.documentation "TRACE"))
 
-((log_level) @debug
-(#eq? @debug "DEBUG"))
+((log_level) @comment.note
+(#eq? @comment.note "DEBUG"))
 
-((log_level) @info
-(#eq? @info "INFO"))
+((log_level) @comment.todo
+(#eq? @comment.todo "INFO"))
 
-((log_level) @warn
-(#eq? @warn "WARN"))
+((log_level) @comment.warning
+(#eq? @comment.warn "WARN"))
 
-((log_level) @error
-(#eq? @error "ERROR"))
+((log_level) @comment.error
+(#eq? @comment.error "ERROR"))
 
-((log_level) @fatal
-(#eq? @fatal "FATAL"))
+((log_level) @label
+(#eq? @label "FATAL"))
 
-(timestamp) @timestamp
-(file_path) @file_path
-(line_number) @line_number
-(message) @message
+(timestamp) @property
+(file_path) @function
+(line_number) @variable
+(message) @string
